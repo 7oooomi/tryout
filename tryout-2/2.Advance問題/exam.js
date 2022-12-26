ca0a3d150b956df0ed91c48cbc870ac2ca8fb186
Naomi Iwamoto <102227130+7oooomi@users.noreply.github.com>
2022-12-26 18:02:04 +0900

    Revert "add tryout3"

--- tryout-2/2.Advance問題/exam.js
+++ tryout-2/2.Advance問題/exam.js
@@ -169,7 +169,13 @@ Q4：
 
 ************************************************************/
 
-function bubbleSort(array) {}
+function bubbleSort(array) {
+  const comparefunc = (a, b) => {
+    return b - a;
+  };
+  array.sort(comparefunc);
+  return array[3];
+}
 
 test('Q4', bubbleSort([6, 9, 2, 1, 0, 3, 7]), 3);
 test('Q4', bubbleSort([7, 10, 3, 1, -6]), 1);
@@ -183,7 +189,7 @@ Q5：
 
 ************************************************************/
 
-function quickSort(array) {}
+// ここに処理を記述
 
 test('Q5', quickSort([6, 9, 2, 1, 0, 3, 7]), 3);
 test('Q5', quickSort([7, 10, 3, 1, -6]), 1);
